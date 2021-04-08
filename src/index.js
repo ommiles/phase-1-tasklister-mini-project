3c2b8459f88651e8966e057dc791be2ae3c3b589
ChristianMFlatiron <chrisrmonteverde@gmail.com>
2021-04-08 12:25:46 -0700

    OM && Chris Code Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+
+  const task = document.getElementById("create-task-form")
+  const list = document.getElementById("tasks")
+  
+  task.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    
+    document.createElement(`list`)
+    
+    const addElement = () => {
+
+      const text = e.target[0].value
+      const grabLi = document.createElement("li");
+      const newLi = grabLi.innerHTML
+
+      list.appendChild(grabLi)
+      
+      grabLi.innerHTML = text
+
+      return newLi
+    }   
+    return addElement()
+  })
+  
 });
